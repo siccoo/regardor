d5efec58984f5e41faa6ea4a22dae22c9221a92b
siccoo <obichilaka@gmail.com>
2019-12-23 14:54:58 +0100

    Asynchronously calling of messages

--- explore.js
+++ explore.js
@@ -1,3 +1,12 @@
 const Twilio = require("twilio");
 
 const client = new Twilio("AC592eed9a95a27823a5dd0a0612a6323f", "7e6e5927a61655aced4e88a906fb771e");
+
+client.messages
+    .list()
+    .then(messages => 
+        console.log(`The most recent message is ${messages[0].body}`)
+);
+
+console.log('Gathering you message log');
+
