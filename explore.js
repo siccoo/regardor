0d371697c80e8f0095ff9ad94118a1e3297ea869
siccoo <obichilaka@gmail.com>
2020-01-07 09:42:32 +0100

    Calling of Twilio api for messages in javascript

--- explore.js
+++ explore.js
@@ -6,7 +6,7 @@ client.messages
     .list()
     .then(messages => 
         console.log(`The most recent message is ${messages[0].body}`)
-);
+).catch(err => console.error(err));
 
 console.log('Gathering you message log');
 
